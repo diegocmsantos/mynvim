6b16b3b045d58e190c38ea3f9946ede04b39fa10
Diego Maia <diegocmsantos@gmail.com>
2024-01-07 14:13:30 +0000

    Comment cmp because of copilot conflict and remove insert.table

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -47,25 +47,26 @@ return {
 				["<C-e>"] = cmp.mapping.abort(),    -- close completion window
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
 
-				["<Tab>"] = cmp.mapping(function(fallback) -- tab to jump to next snippet's placeholder
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-
-				["<S-Tab>"] = cmp.mapping(function(fallback) -- shift-tab to jump to previous snippet's placeholder
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
+				-- THIS IS CONFLICTING WITH COPILOT
+				-- ["<Tab>"] = cmp.mapping(function(fallback) -- tab to jump to next snippet's placeholder
+				-- 	if cmp.visible() then
+				-- 		cmp.select_next_item()
+				-- 	elseif luasnip.expand_or_jumpable() then
+				-- 		luasnip.expand_or_jump()
+				-- 	else
+				-- 		fallback()
+				-- 	end
+				-- end, { "i", "s" }),
+				--
+				-- ["<S-Tab>"] = cmp.mapping(function(fallback) -- shift-tab to jump to previous snippet's placeholder
+				-- 	if cmp.visible() then
+				-- 		cmp.select_prev_item()
+				-- 	elseif luasnip.jumpable(-1) then
+				-- 		luasnip.jump(-1)
+				-- 	else
+				-- 		fallback()
+				-- 	end
+				-- end, { "i", "s" }),
 			}),
 
 			-- Order of sources determines order of sourcing
